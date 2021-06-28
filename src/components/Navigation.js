113c7fc31965bc31e5decd4c82e142e08625f18f
taylorsieling <taylorasieling@gmail.com>
2021-06-28 15:56:21 -0500

    building nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,16 @@ import React from 'react'
 export default function Navigation() {
     return (
         <section id="navigation">
-            <div className="top-nav">
-
+            <div className="topnav">
+                <p>Taylor Sieling</p>
+                <a href="#contact">Contact</a>
+                <a href="#resume">Résumé</a>
+                <a href="#blog">Blog</a>
+                <a href="#projects">Projects</a>
+                <a href="#about">About</a>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars fa-2x"></i>
+                </a>
             </div>
         </section>
     )
