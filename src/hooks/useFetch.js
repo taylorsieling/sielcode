404f0e0f0fe965b9bb79df3abf3e24a26e22b4e0
taylorsieling <taylorasieling@gmail.com>
2021-06-28 11:28:07 -0500

    building blog fetch & comp

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,18 @@
+function useFetch(url, opts) {
+    const [response, setResponse] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [hasError, setHasError] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        fetch(url, opts)
+            .then((res) => {
+            setResponse(res.data)
+            setLoading(false)
+        })
+            .catch(() => {
+                setHasError(true)
+                setLoading(false)
+            })
+    }, [ url ])
+    return [ response, loading, hasError ]
+}
