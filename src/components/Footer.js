664d49042b57a7757d1c9484a977525150176ec8
taylorsieling <taylorasieling@gmail.com>
2021-06-29 10:31:19 -0500

    building footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function Footer() {
+function Footer() {
     return (
-        <div>
-            <p>I am the Footer Component</p>
-        </div>
+        <section className="footer">
+            <div className="footer">
+                &copy; 2021 Taylor Sieling 
+                <span className="right footnav">
+                    <a href="mailto:taylorasieling@gmail.com">Email</a>&nbsp;&#47;&#47;&nbsp;
+                    <a href="https://www.linkedin.com/in/taylorasieling/">LinkedIn</a>&nbsp;&#47;&#47;&nbsp; 
+                    <a href="https://twitter.com/sielcode">Twitter</a>
+                </span>
+            </div>
+        </section>
     )
 }
+
+export default Footer;
