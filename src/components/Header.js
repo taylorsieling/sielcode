c5940476b6872276d85c14342d29181fc81e613e
taylorsieling <taylorasieling@gmail.com>
2021-07-09 21:54:07 -0500

    designing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,16 @@ export default function Header() {
         <section id="header">
             <div className="header">
                 <div className="row">
-                    <div className="row-two header-one">
-                        <h1 className="main-title">Hello World.<br/>I'm Taylor.</h1>
-                    </div>
                     <div className="row-two">
                         <img className="headshot" src={headshot} alt="Taylor's Headshot"/>
                     </div>
+                    <div className="row-two header-one">
+                        <h1 className="main-title">Oh hey, I'm Tay.</h1>
+                        <h1 className="main-title">Let's write some code.</h1>
+                    </div>
                 </div>
             </div>
+            <div className="spacer"></div>
         </section>
     )
 }
