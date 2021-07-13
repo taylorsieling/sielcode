01fb2bae73133ad566b89708018c0bb27d5af11b
taylorsieling <taylorasieling@gmail.com>
2021-07-13 15:50:09 -0500

    building contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import contactpic from '../images/contactpic.png'
 
 export default function Contact() {
     return (
@@ -6,11 +7,16 @@ export default function Contact() {
             <div className="contact">
                 <div className="contact-div">
                     <div className="row">
-                        <div className="row-two contact-title">Let's Stay in Touch!</div>
-                        <div className="row-two">
-                            <p><a href="#" className="button">Email</a></p>
-                            <p><a href="#" className="button">LinkedIn</a></p>
-                            <p><a href="#" className="button">Twitter</a></p>
+                        <div className="row-two-con"><img src={contactpic} alt="headshot" className="contact-pic"/></div>
+                        <div className="contact">
+                            <p className="contact-title">Let's Stay in Touch!</p>                            
+                            <p className="icon">
+                                <a href="#" ><i class="fas fa-envelope-square fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp; 
+                                <a href="#" ><i class="fa fa-twitter-square fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                                <a href="#" ><i class="fab fa-dev fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp; 
+                                <a href="#" ><i class="fab fa-github-square fa-3x"></i></a>
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ export default function Header() {
         <section id="header">
             <div className="header">
                 <div className="row">
-                    <div className="row-two header-img">
+                    <div className="row-two-head header-img">
                         <img className="headshot" src={headshot} alt="Taylor's Headshot"/>
                     </div>
-                    <div className="row-two header-title">
+                    <div className="row-two-head header-title">
                         <h1 className="main-title">Oh hey, I'm Tay.</h1>
                         <h1 className="main-title">Let's write some code.</h1>
                     </div>
