dbd69853641c16022d51c3aaa0f6381c689fdb91
taylorsieling <taylorasieling@gmail.com>
2021-07-13 09:55:33 -0500

    updating projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Plantd from '../images/Plantd.png'
-import PenciledIn from '../images/PenciledIn.png'
 import KKF from '../images/KKF.png'
 
 function Projects() {
@@ -11,17 +9,17 @@ function Projects() {
             </div>
             <hr/>
         <div className="row" id="projects">
-            <div className="row-three">
+            {/* <div className="row-three">
                 <div className="card-header">
                     <img className="card-img" src="https://images.unsplash.com/photo-1509395062183-67c5ad6faff9?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZGV2ZWxvcGVyfGVufDB8MnwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60" alt="fasdfdf"/>
                 </div>
                 <div className="card-body">
                     <p><strong>Swiftify: </strong>Rate every Taylor Swift song. View your stats. Compare with friends.</p>
                 </div>
-            </div>
+            </div> */}
             <div className="row-three">
                 <div className="card-header">
-                    <img className="card-img" src={KKF} alt="adfsdf"/>
+                    <img className="card-img" src="https://i.imgur.com/fU70IZ0.jpg?1" alt="adfsdf"/>
                 </div>
                 <div className="card-body">
                     <p><strong>The Kitten Foster: </strong>Record your kitten foster intake information. Update it as they grow.</p>
@@ -29,10 +27,18 @@ function Projects() {
             </div>
             <div className="row-three">
                 <div className="card-header">
-                    <img className="card-img" src={Plantd} alt="fdasfas"/>
+                    <img className="card-img" src="https://images.unsplash.com/photo-1529340376075-067b25b3d7fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="fdasfas"/>
                 </div>
                 <div className="card-body">
                     <p><strong>Plantd: </strong>For the forgetful plant parent - Add your houseplants. Monitor their care.</p>
+                </div>
+            </div>
+            <div className="row-three">
+                <div className="card-header">
+                    <img className="card-img" src="https://images.unsplash.com/photo-1559881231-1010b6f3a96a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" alt="adfsdf"/>
+                </div>
+                <div className="card-body">
+                    <p><strong>Penciled In: </strong>For event planners to advertise their events, and event goers to find them.</p>
                 </div>
             </div>
         </div>
